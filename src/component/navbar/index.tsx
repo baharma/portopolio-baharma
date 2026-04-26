e2acfee800956a5bf01b4e4bd2ed0891a53c8958
baharma <bagusaditya1899@gmail.com>
2026-04-27 04:46:58 +0800

    chore:add setting vscode went save will be clear line

--- src/component/navbar/index.tsx
+++ src/component/navbar/index.tsx
@@ -1,7 +1,14 @@
 export default function ComponentsNavbar() {
-  return (
-    <div className="bg-gray-800 text-white p-4">
-        
-    </div>
-  );
+    return (
+        <div className="bg-gray-100  p-4 flex justify-center items-center ">
+            <div className="flex justify-between">
+                <div className="flex gap-3">
+                    <div className="border-2  shadow text-zinc-400 rounded-2xl p-2 text-2xl">Baharma</div>
+                </div>
+                <div className="">
+                    asdasd
+                </div>
+            </div>
+        </div>
+    );
 }
