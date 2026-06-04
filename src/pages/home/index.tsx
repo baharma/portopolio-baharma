6183e327bb45dbbd083d037e0e6f596b341f0541
baharma <bagusaditya1899@gmail.com>
2026-06-04 10:44:08 +0800

    fix: update navbar styles and clean up home page return statement

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,5 +1,5 @@
 export default function PagesHome() {
-    return(
+    return (
         <div>
             <h1>Welcome to the Home Page</h1>
             <p>This is a simple home page component.</p>
