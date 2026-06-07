448f44f8877e8ef69dc2e83c3767571772d65d17
baharma <bagusaditya1899@gmail.com>
2026-06-07 20:35:15 +0800

    feat: install package library icon react icon and fix navbar to can show handle mobile device

--- src/component/button-underline/index.tsx
+++ src/component/button-underline/index.tsx
@@ -1,6 +1,6 @@
 export default function ComponentsButtonUnderline({ title }: { title: string }) {
     return (
-        <div className="group relative w-fit cursor-pointer pb-1">
+        <div className="group relative w-fit cursor-pointer pb-1 font-mono hover:-translate-y-2 transition-transform " >
             {title}
             <span className="absolute bottom-0 left-0 h-[2px] w-0 bg-black transition-all duration-300 ease-in-out group-hover:w-full"></span>
         </div>
--- src/component/navbar/index.tsx
+++ src/component/navbar/index.tsx
@@ -1,30 +1,77 @@
+"use client"
+
+import { SlArrowDown, SlArrowUp } from "react-icons/sl";
 import ComponentsButtonUnderline from "../button-underline";
 import ComponentsLiveIndicator from "../live-indicator";
+import { useEffect, useState } from "react";
 
 export default function ComponentsNavbar() {
+    const [buttonActive, setButtonActive] = useState<boolean>(false);
+    useEffect(() => {
+        // Fungsi untuk mengecek lebar layar
+        const handleResize = () => {
+            // window.innerWidth membaca ukuran lebar layar saat ini
+            if (window.innerWidth <= 700) {
+                setButtonActive(false); // Paksa tertutup/false jika layar 700px ke bawah
+            }
+        };
+
+        // Panggil fungsinya sekali saat komponen pertama kali dimuat
+        handleResize();
+
+        // Pasang "pendengar" (event listener) setiap kali ukuran layar ditarik/diubah
+        window.addEventListener("resize", handleResize);
+
+        // Cleanup: Hapus event listener saat komponen tidak lagi digunakan agar tidak memori bocor (memory leak)
+        return () => window.removeEventListener("resize", handleResize);
+    }, []);
     return (
         <div className="bg-secondary border-b border-gray-200">
-            <div className="max-w-7xl mx-auto p-4 flex items-center justify-between w-full ">
-                <div className="flex gap-3 ">
-                    <div className="flex items-center">
+            <div className="max-w-7xl mx-auto p-6 flex items-center justify-between w-full ">
+                <div className="flex gap-3 items-center">
+                    <div className="flex items-center justify-center">
                         <ComponentsLiveIndicator />
                         <div className="ml-2 text-xl  font-instrument-serif ">
                             BAHARMA.MY.ID
                         </div>
                     </div>
                 </div>
-                <div className="flex gap-10">
+                <div className="hidden md:flex gap-10 ">
                     <ComponentsButtonUnderline title="Work History" />
                     <ComponentsButtonUnderline title="About" />
                     <ComponentsButtonUnderline title="Projects" />
                     <ComponentsButtonUnderline title="Contacts" />
                 </div>
-                <div className="flex items-center gap-3">
-                    <button className="px-4 py-2 bg-primary text-white rounded-2xl hover:bg-primary/90 transition-colors">
+                <div className="hidden md:flex items-center gap-3 ">
+                    <button className="px-4 py-2 bg-primary text-white rounded-2xl hover:bg-primary/90 transition-colors font-sans">
                         Download CV
                     </button>
                 </div>
+                <div className=" lg:hidden flex items-center gap-3 ">
+                    <button className="px-4 py-2  cursor-pointer  rounded-2xl" onClick={() => setButtonActive(!buttonActive)}>
+                        {
+                            buttonActive ? (
+                                <SlArrowUp className="text-2xl" />
+                            ) : (
+                                <SlArrowDown className="text-2xl" />
+                            )
+                        }
+                    </button>
+                </div>
             </div>
+            {buttonActive && (
+                <div className="lg:hidden relative top-full left-0 w-full bg-secondary border-b border-gray-200 shadow-lg flex flex-col p-6 gap-6">
+                    <div className="flex flex-col gap-4">
+                        <ComponentsButtonUnderline title="Work History" />
+                        <ComponentsButtonUnderline title="About" />
+                        <ComponentsButtonUnderline title="Projects" />
+                        <ComponentsButtonUnderline title="Contacts" />
+                    </div>
+                    <button className="w-full py-3 bg-primary text-white rounded-2xl hover:bg-primary/90 transition-colors font-sans text-center mt-2">
+                        Download CV
+                    </button>
+                </div>
+            )}
         </div>
     );
 }
